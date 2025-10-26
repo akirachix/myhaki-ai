aec594254f3fdd056f70f11f63543cb96a428ad5
student <lwambsrat@gmail.com>
2025-10-26 10:36:11 +0300

    feat:implement docker

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -3,11 +3,11 @@ from sentence_transformers import SentenceTransformer
 from google.generativeai import GenerativeModel, configure
 from datetime import datetime
 import os
-from dotenv import load_dotenv
 from supabase import create_client
 import functools
-
+from dotenv import load_dotenv
 load_dotenv()
+
 
 api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
